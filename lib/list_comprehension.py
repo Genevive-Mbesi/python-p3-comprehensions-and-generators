a1be667f596047c2aa44f36e1dc65aaa4cd42528
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-08-25 09:33:33 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,8 @@
 
 def return_evens(num_list):
     pass
+    return [even for even in num_list if even % 2 == 0]
 
 def make_exclamation(sentence_list):
     pass
+    return [f"{string}!" for string in sentence_list]
